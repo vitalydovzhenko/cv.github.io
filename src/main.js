c76eed7647f6ab41276e2887db2a2205ab58209a
Vitalii Dovzhenko <vitaly.dovzhenko@gmail.com>
2023-10-30 10:12:48 +0200

    fix gradient

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 window.addEventListener('mousemove', function (e) {
     const w = window.innerWidth;
     const h = window.innerHeight;
-    document.documentElement.style.setProperty('--mouse-x', 100 / w * e.clientX + '%');
-    document.documentElement.style.setProperty('--mouse-y', 100 / h * e.clientY + '%');
+    document.documentElement.style.setProperty('--mouse-x', 100 / w * e.clientX + 'px');
+    document.documentElement.style.setProperty('--mouse-y', 100 / h * e.clientY + 'px');
 });
